b57e36b1ac3ed9015530fc251ccae5c82820d64f
cccs-nik <68961854+cccs-nik@users.noreply.github.com>
2025-05-23 17:47:46 -0400

    maybe this idk

--- gateway-ha/src/main/java/io/trino/gateway/ha/security/SessionCookie.java
+++ gateway-ha/src/main/java/io/trino/gateway/ha/security/SessionCookie.java
@@ -41,7 +41,7 @@ public final class SessionCookie
                 .comment("")
                 .maxAge(60 * 60 * 24)
                 .secure(true)
-                .httpOnly(false)
+                .httpOnly(true)
                 .sameSite(NONE)
                 .build();
 
